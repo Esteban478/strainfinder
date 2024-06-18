c24d1a12caf1757073ef96c539d456099618ba5b
Esteban478 <157967976+Esteban478@users.noreply.github.com>
2024-06-18 13:03:01 +0200

    connect api, add basic functionality + styles

---
+++ main.js
@@ -0,0 +1,59 @@
+let strainData = [];
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('https://strainsapi.netlify.app/strains.json')
+        .then(response => response.json())
+        .then(data => {
+            strainData = data;
+        })
+        .catch(error => console.error('Error fetching data:', error));
+});
+
+const searchInput = document.querySelector('#search');
+
+searchInput.addEventListener('input', (event) => {
+    const searchText = event.target.value.toLowerCase();
+    if (searchText === '') {
+        // Clear previous results
+        const resultContainer = document.querySelector('.result-container');
+        resultContainer.innerHTML = '';
+        return;
+    }
+    const filteredData = strainData.filter(strain => {
+        // return (strain.name && typeof strain.name === 'string' && strain.name.toLowerCase().includes(searchText)) ||
+        //     (strain.description && typeof strain.description === 'string' && strain.description.toLowerCase().includes(searchText));
+        return (strain.name && typeof strain.name === 'string' && strain.name.toLowerCase().includes(searchText));
+    });
+
+    const resultContainer = document.querySelector('.result-container');
+    resultContainer.innerHTML = ''; // Clear previous results
+
+    filteredData.forEach(strain => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        const image = document.createElement('img');
+        image.src = strain.img_url ? strain.img_url : './assets/strainfinder_400.jpeg';
+        image.width = 250;
+        card.appendChild(image);
+
+        const name = document.createElement('h3');
+        name.textContent = strain.name;
+        card.appendChild(name);
+
+        const type = document.createElement('p');
+        type.textContent = `Type: ${strain.type}`;
+        card.appendChild(type);
+
+        const thcContent = document.createElement('p');
+        thcContent.textContent = `THC: ${strain.thc_level}`;
+        card.appendChild(thcContent);
+
+        const description = document.createElement('p');
+        description.classList.add('description');
+        description.textContent = strain.description;
+        card.appendChild(description);
+
+        resultContainer.appendChild(card);
+    });
+});
